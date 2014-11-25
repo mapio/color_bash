e25cd030a14e5e69e1b1ab824b61d13155393ea5
Pierlauro <pierlauro.sciarelli@studenti.unimi.it>
2014-11-26 00:50:29 +0100

    Delete old .bashrc lines

--- colorBash.sh
+++ colorBash.sh
@@ -11,7 +11,7 @@ white_bold='\e[1;37m'
 echo -e $red"Bash Color Changer v1.0"
 echo -e $green"Copyright (C) 2014 - Pierlauro Sciarelli$white\n"
 
-echo -e "LEGENDA: "$red"red "$green"green "$orange"orange "$blue"blue "$purple"purple "$cyan"cyan "$white"white\n"
+echo -e "LEGEND: "$red"red "$green"green "$orange"orange "$blue"blue "$purple"purple "$cyan"cyan "$white"white\n"
 echo -e $white"Your prompt is going to be like: $white_bold"$USER"@"$HOSTNAME" current_directory $\n"
 echo -e $white"What color you want $white_bold$USER"$white?
 read user
@@ -24,5 +24,6 @@ read dir
 echo -e "What color you want $ ?"
 read dollar
 
-echo export PS1=\"${!user}"\u"${!et}"@"${!host}"\h "${!dir}"\W"${!dollar}" $\e[m "\" "## Line added by ColorBash">> ~/.bashrc
+sed -i '/export\(.*\)##Line added by ColorBash/d' ~/.bashrc
+echo export PS1=\"${!user}"\u"${!et}"@"${!host}"\h "${!dir}"\W"${!dollar}" $\e[m "\" "##Line added by ColorBash">> ~/.bashrc
 exec bash
