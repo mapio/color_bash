c9ea23ba6150f5a1ca0df5076ab83e6d76df5c12
Pierlauro <pierlauro.sciarelli@studenti.unimi.it>
2014-11-26 01:04:05 +0100

    Colors array

--- colorBash.sh
+++ colorBash.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-red='\e[0;31m'
-green='\e[0;32m'
-orange='\e[0;33m'
-blue='\e[0;34m'
-purple='\e[0;35m'
-cyan='\e[0;36m'
-white='\e[0;38m'
-white_bold='\e[1;37m'
+declare -A colors
+colors=([red]='\e[0;31m' [green]='\e[0;32m' [orange]='\e[0;33m' [blue]='\e[0;34m' [purple]='\e[0;35m' [cyan]='\e[0;36m' [white]='\e[0;38m' [white_bold]='\e[1;37m')
 
-echo -e $red"Bash Color Changer v1.0"
-echo -e $green"Copyright (C) 2014 - Pierlauro Sciarelli$white\n"
+echo -e ${colors[red]}"Bash Color Changer v1.0"
+echo -e ${colors[green]}"Copyright (C) 2014 - Pierlauro Sciarelli${colors[white]}\n"
 
-echo -e "LEGEND: "$red"red "$green"green "$orange"orange "$blue"blue "$purple"purple "$cyan"cyan "$white"white\n"
-echo -e $white"Your prompt is going to be like: $white_bold"$USER"@"$HOSTNAME" current_directory $\n"
-echo -e $white"What color you want $white_bold$USER"$white?
+echo -e "LEGEND: "${colors[red]}"red "${colors[green]}"green "${colors[orange]}"orange "${colors[blue]}"blue "${colors[purple]}"purple "${colors[cyan]}"cyan "${colors[white]}"white\n"
+echo -e ${colors[white]}"Your prompt is going to be like: ${colors[white_bold]}"$USER"@"$HOSTNAME" current_directory $\n"
+echo -e ${colors[white]}"What color you want ${colors[white_bold]}$USER"${colors[white]}?
 read user
-echo -e $white"What color you want $white_bold"@"$white?"
+echo -e ${colors[white]}"What color you want ${colors[white_bold]}"@"${colors[white]}?"
 read et
-echo -e "What color you want $white_bold$HOSTNAME$white?"
+echo -e "What color you want ${colors[white_bold]}$HOSTNAME${colors[white]}?"
 read host
-echo -e "What color you want "$white_bold"current_directory"$white"?"
+echo -e "What color you want "${colors[white_bold]}"current_directory"${colors[white]}"?"
 read dir
 echo -e "What color you want $ ?"
 read dollar
 
 sed -i '/export\(.*\)##Line added by ColorBash/d' ~/.bashrc
-echo export PS1=\"${!user}"\u"${!et}"@"${!host}"\h "${!dir}"\W"${!dollar}" $\e[m "\" "##Line added by ColorBash">> ~/.bashrc
+echo export PS1=\"${colors[$user]}"\u"${colors[$et]}"@"${colors[$host]}"\h "${colors[$dir]}"\W"${colors[$dollar]}" $\e[m "\" "##Line added by ColorBash">> ~/.bashrc
 exec bash
